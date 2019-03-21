e421973f740fd075a9b2a6dee20e36cd2856f84c
Dominic Yegon <domieyegon70@gmail.com>
2019-03-21 18:10:22 +0300

    This a practise on google maps

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gMapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gMapApp
+ */
+angular.module('gMapApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
